f9d7ba73db173b6a6a8cb8940163e3d14710841e
freek <freek@spatie.be>
2016-12-26 22:09:40 +0100

    add tests

--- __tests__/errors.test.js
+++ __tests__/errors.test.js
@@ -9,12 +9,34 @@ describe('Errors', () => {
         errors = new Errors();
     });
 
-
     it('can determine if there are any errors', () => {
         assert.isFalse(errors.any());
 
-        errors.record({'first_name': 'Value is required'})
+        errors.record({'first_name': ['Value is required']})
 
         assert.isTrue(errors.any());
+    });
+
+    it('can get a specific error', () => {
+        assert.isFalse(errors.any());
+
+        errors.record({'first_name': ['Value is required']})
+
+        assert.equal('Value is required', errors.get('first_name'));
+
+        assert.isUndefined(errors.get('last_name'));
+    });
+
+    it('can clear all the errors', () => {
+        errors.record({
+            'first_name': ['Value is required'],
+            'last_name': ['Value is required'],
+        })
+
+        assert.isTrue(errors.any());
+
+        errors.clear();
+
+        assert.isFalse(errors.any());
     });
 });
